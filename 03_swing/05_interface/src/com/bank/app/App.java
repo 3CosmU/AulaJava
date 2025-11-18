1d4585215019b5d3f3cab018187e4dbc60b7277b
Wagner Moreira <wagnercosmu@gmail.com>
2025-11-17 21:34:48 -0300

    Aula 17/11

--- 03_swing/05_interface/src/com/bank/app/App.java
+++ 03_swing/05_interface/src/com/bank/app/App.java
@@ -4,67 +4,68 @@ import javax.swing.JOptionPane;
 import com.bank.models.Conta;
 
 public class App {
-    public static void main(String[] args) throws Exception {
-        // Instnacia
-        Conta cc = new Conta(null,
-                null,
-                "1234-5",
-                "12345-6",
-                0.0);
+        public static void main(String[] args) throws Exception {
+                // Instnacia
+                Conta cc = new Conta(null,
+                                null,
+                                "1234-5",
+                                "12345-6",
+                                0.0);
 
-        // Array / variavel
-        String[] opcoes = { "Consultar dados",
-                "Depositar",
-                "Sacar",
-                "Sair do programa" };
+                // Array / variavel
+                String[] opcoes = { "Consultar dados",
+                                "Depositar",
+                                "Sacar",
+                                "Sair do programa" };
 
-        Object opcao;
-        double valor;
+                Object opcao;
+                double valor;
 
-        // Entrada de dados
-        cc.setTitular(JOptionPane.showInputDialog("Informe o nome do titular da conta: "));
-        cc.setCpf(JOptionPane.showInputDialog("Informe o Cpf da conta: "));
-        
-        do {
+                // Entrada de dados
+                cc.setTitular(JOptionPane.showInputDialog("Informe o nome do titular da conta: "));
+                cc.setCpf(JOptionPane.showInputDialog("Informe o Cpf da conta: "));
 
-            opcao = JOptionPane.showInternalInputDialog(
-                    null,
-                    "Escolha a opção desejada",
-                    null,
-                    JOptionPane.QUESTION_MESSAGE,
-                    null,
-                    opcoes,
-                    opcoes[0]);
+                do {
 
-            if (opcao == "Consultar dados") {
-                JOptionPane.showMessageDialog(null, cc.consultarDados());
-            } else if (opcao == "Depositar") {
-                valor = Double.parseDouble(
-                        JOptionPane.showInputDialog("Informe o valor do deposito em R$: ").replace(",", "."));
-                JOptionPane.showMessageDialog(null, "Deposito efetuado com sucesso. \nValor do saldo: R$ "
-                        + String.format("%.2f", cc.depositar(valor)));
-            }
+                        opcao = JOptionPane.showInputDialog(
+                                        null,
+                                        "Escolha a opção desejada",
+                                        null,
+                                        JOptionPane.QUESTION_MESSAGE,
+                                        null,
+                                        opcoes,
+                                        opcoes[0]);
 
-            else if (opcao == "Sacar") {
-                valor = Double.parseDouble(
-                        JOptionPane.showInputDialog(("Informe o valor do saque em R%: ")).replace(",", "."));
-                if (cc.getSaldo() >= valor) {
-                    JOptionPane.showMessageDialog(null,
-                            "Saque efetuado com sucesso." +
-                                    "\nValor do saldo: R$ " +
-                                    String.format("%.2f", cc.sacar(valor)));
-                } else {
-                    JOptionPane.showMessageDialog(null,
-                            "Saldo Insuficiente" +
-                                    "\nSaldo atual: R$" +
-                                    String.format("%.2f", cc.getSaldo()));
-                }
-
-            }
-
-        } while (opcao != "Sair");
-
-    }
+                        if (opcao == "Consultar dados") {
+                                JOptionPane.showMessageDialog(null, cc.consultarDados());
+                        } else if (opcao == "Depositar") {
+                                valor = Double.parseDouble(
+                                                JOptionPane.showInputDialog("Informe o valor do depósito em R$: ")
+                                                                .replace(",", "."));
+                                JOptionPane.showMessageDialog(
+                                                null,
+                                                "Depósito efetuado com sucesso." +
+                                                                "\nValor do saldo: R$ " +
+                                                                String.format("%.2f", cc.depositar(valor)));
+                        } else if (opcao == "Sacar") {
+                                valor = Double.parseDouble(JOptionPane
+                                                .showInputDialog("Informe o valor do saque em R$: ").replace(",", "."));
+                                if (cc.getSaldo() >= valor) {
+                                        JOptionPane.showMessageDialog(
+                                                        null,
+                                                        "Saque efetuado com sucesso." +
+                                                                        "\nValor do saldo: R$ " +
+                                                                        String.format("%.2f", cc.sacar(valor)));
+                                } else {
+                                        JOptionPane.showMessageDialog(
+                                                        null,
+                                                        "Saldo insuficiente." +
+                                                                        "\nSaldo atual: R$ " +
+                                                                        String.format("%.2f", cc.getSaldo()));
+                                }
+                        }
+                } while (opcao != "Sair");
+        }
 }
 
 // Criar um app do banco,onde o usuario vai abrir e criar uma conta no banco,
