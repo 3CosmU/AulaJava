0a235311313589f20e9af695591d4fe2e23face0
Wagner Moreira <wagnercosmu@gmail.com>
2025-12-11 21:39:49 -0300

    Aula 11/12

--- 04_front_end/13_mascara/js/js.js
+++ 04_front_end/13_mascara/js/js.js
@@ -13,13 +13,13 @@ const exibirInfo = () => {
     let endereco = document.querySelector("#endereco").value;
 
     //saida de dados
-    document.querySelector('#nomeResult').innerHTML = `<br>${nome}<br><br>`;
-    document.querySelector('#emailResult').innerHTML = `<b>E-mail:</b> ${email}<br>`;
-    document.querySelector('#nascimentoResult').innerHTML = `<b>Data de Nascimento:</b> ${nascimento}<br>`;
-    document.querySelector('#cpfResult').innerHTML = `<b>CPF:</b> ${cpf}<br>`;
-    document.querySelector('#telefoneResult').innerHTML = `<b>Telefone:</b> ${telefone}<br>`;
-    document.querySelector('#cepResult').innerHTML = `<b>CEP:</b> ${cep}`;
-    document.querySelector('#enderecoResult').innerHTML = `<b>Endereço:</b> ${endereco}<br>`;
+    document.querySelector('#nomeResult').innerHTML = `<br>${nome}<br><br><hr>`;
+    document.querySelector('#emailResult').innerHTML = `<b>E-mail:</b> ${email}<br><hr>`;
+    document.querySelector('#nascimentoResult').innerHTML = `<b>Data de Nascimento:</b> ${nascimento}<br><hr>`;
+    document.querySelector('#cpfResult').innerHTML = `<b>CPF:</b> ${cpf}<br><hr>`;
+    document.querySelector('#telefoneResult').innerHTML = `<b>Telefone:</b> ${telefone}<br><hr>`;
+    document.querySelector('#cepResult').innerHTML = `<b>CEP:</b> ${cep}<br><hr>`;
+    document.querySelector('#enderecoResult').innerHTML = `<b>Endereço:</b> ${endereco}<br><hr>`;
 
 
     //apagar dados de todo o formulario
